7607f779a906c12e83b7d8c2a0047b244243361e
ravisinghup <er.ravi.ki@gmail.com>
2025-02-09 19:15:58 +0530

    added security group

--- security_group.tf
+++ security_group.tf
@@ -15,8 +15,3 @@ resource "aws_vpc_security_group_ingress_rule" "Irule1" {
     ip_protocol = "tcp"
     to_port = 443
 }
-resource "aws_vpc_security_group_egress_rule" "Erule1" {
-  security_group_id = aws_security_group.terra-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
