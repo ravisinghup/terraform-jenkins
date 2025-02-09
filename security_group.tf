7b5696a4faf9aa4714ff1c8512ba93a994df8a6f
rajatsingh00998 <rajatsingh00998@gmail.com>
2025-02-10 00:20:26 +0530

    removed sg

--- security_group.tf
+++ security_group.tf
@@ -1,17 +1,17 @@
-resource "aws_security_group" "terra-sg" {
-    name = "terra-sg"
-    description = "Creating Security group using Jenkins"
-    vpc_id = "aws_vpc.terra-demo.id"
+# resource "aws_security_group" "terra-sg" {
+#     name = "terra-sg"
+#     description = "Creating Security group using Jenkins"
+#     vpc_id = "aws_vpc.terra-demo.id"
 
-    tags = {
-        Name = "terra-sg"
-    }
-}
+#     tags = {
+#         Name = "terra-sg"
+#     }
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "Irule1" {
-    security_group_id = aws_security_group.terra-sg.id
-    cidr_ipv4 = aws_vpc.terra-demo.cidr_block
-    from_port = 443
-    ip_protocol = "tcp"
-    to_port = 443
-}
+# resource "aws_vpc_security_group_ingress_rule" "Irule1" {
+#     security_group_id = aws_security_group.terra-sg.id
+#     cidr_ipv4 = aws_vpc.terra-demo.cidr_block
+#     from_port = 443
+#     ip_protocol = "tcp"
+#     to_port = 443
+# }
