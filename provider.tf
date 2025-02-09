27e1b90e85db199b6c058f68177fe7d84f0cdfbf
ravisinghup <40655865+ravisinghup@users.noreply.github.com>
2025-02-09 18:31:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws-access-key-id
+  secret_key = var.aws-secret-access-key
   region     = var.aws_region
 }
